7b3059ff64dcf5d64702473455788f592fbd6c1b
geraldc118 <geraldcameron118@gmail.com>
2021-10-27 13:49:49 +0100

    ading local port

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
