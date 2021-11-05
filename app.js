582e501a34b770223dfb1096e2dd302e971bff2a
geraldc118 <geraldcameron118@gmail.com>
2021-11-05 14:17:12 +0000

    removing mongoose-encryption

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+
 
 require('dotenv').config();
 
